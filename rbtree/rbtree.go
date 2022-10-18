f75fbe12020c713470d00ba0a91c6e5c3f7cb535
holykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-18 21:02:21 +0800

    添加 get 方法

--- rbtree/rbtree.go
+++ rbtree/rbtree.go
@@ -20,3 +20,34 @@ type Node struct {
 type Tree struct {
 	root *Node
 }
+
+func isRed(x *Node) bool {
+	if x == nil {
+		return false
+	}
+	return x.color == RED
+}
+
+func (t *Tree) get(key bst.Key) (interface{}, error) {
+	if key == nil {
+		return nil, bst.ErrInvalidArgument
+	}
+	return get(t.root, key)
+}
+
+func get(x *Node, key bst.Key) (interface{}, error) {
+	for {
+		if x != nil {
+			break
+		}
+		cmp := key.CompareTo(x.key)
+		if cmp < 0 {
+			x = x.left
+		} else if cmp > 0 {
+			x = x.right
+		} else {
+			return x.val, nil
+		}
+	}
+	return nil, nil
+}
