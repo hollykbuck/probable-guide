10db1db26f9e67a22a1d0aa2af5ecebec4ba3334
holykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-18 21:15:51 +0800

    准备实现红黑树的 delete 方法

--- rbtree/rbtree.go
+++ rbtree/rbtree.go
@@ -3,7 +3,9 @@ package rbtree
 import "github.com/hollykbuck/probable-guide/bst"
 
 const (
-	RED   = true
+	// RED 红表示 (2,4) B 树的关键字（额外）
+	RED = true
+	// BLACK 黑表示 (2,4) B 树的节点
 	BLACK = false
 )
 
@@ -50,4 +52,51 @@ func get(x *Node, key bst.Key) (interface{}, error) {
 		}
 	}
 	return nil, nil
+}
+
+func (t *Tree) delete(key bst.Key) error {
+	if key == nil {
+		return bst.ErrInvalidArgument
+	}
+	contains, err := t.contains(key)
+	if err != nil {
+		return err
+	}
+	// 如果不包含，直接结束
+	if !contains {
+		return nil
+	}
+	// 如果左右孩子都是黑，
+	if !isRed(t.root.left) && !isRed(t.root.right) {
+		t.root.color = RED
+	}
+	t.root = deleteFromNode(t.root, key)
+	if !t.isEmpty() {
+		t.root.color = BLACK
+	}
+	return nil
+}
+
+func deleteFromNode(h *Node, key bst.Key) *Node {
+	if key.CompareTo(h.key) < 0 {
+		if !isRed(h.left) && !isRed(h.left.left) {
+			h = moveRedLeft(h)
+		}
+	}
+}
+
+func moveRedLeft(h *Node) *Node {
+
+}
+
+func (t *Tree) contains(key bst.Key) (bool, error) {
+	i, err := t.get(key)
+	if err != nil {
+		return false, err
+	}
+	return i != nil, nil
+}
+
+func (t *Tree) isEmpty() bool {
+	return t.root == nil
 }
